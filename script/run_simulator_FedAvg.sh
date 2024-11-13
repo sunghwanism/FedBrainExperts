54a3110950464ca50aac77e607c9a59976fe5a77
SHMoon <msh2044@gmail.com>
2024-11-13 17:34:25 +0900

    upload new script

--- script/run_simulator_FedAvg.sh
+++ script/run_simulator_FedAvg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-epochs=(1 10 20)
+seeds=(10 20 30 40)
 
-for epoch in "${epochs[@]}"
+for seed in "${seeds[@]}"
 do
-    python src/simulator/FLTrainer.py --device_id 0 --agg_method FedAvg --batch_size 64 --epoch "$epoch"
+    python src/simulator/FLTrainer.py --device_id 1 --agg_method FedAvg --wandb_project Fine_FL_Thesis_v5 --optimizer sgd --lr 2e-6 --batch_size 64 --seed "$seed"
     sleep 60
 done
